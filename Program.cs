41f4ae1bec597c616248a5e565f4fff883c2a185
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-04 12:14:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Call_By_value_And_Call_by_reference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 40;
+            callbyvalue1(i);
+            callbyreference1(ref j);
+
+
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+
+        }
+        public static void callbyvalue1(int j)
+        {
+            j = 20;
+        }
+        public static void callbyreference1(ref int j)
+        {
+            j = 30;
+        }
+
+    }
+}
